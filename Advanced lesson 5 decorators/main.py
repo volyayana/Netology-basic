bb8b35a2e90e7b76cb691202af9e5c64b1bdf8e6
volyayana <volya_y@mail.ru>
2022-01-12 21:39:11 +0300

    1. Написать декоратор - логгер. Он записывает в файл дату и время вызова функции, имя функции, аргументы, с которыми вызвалась и возвращаемое значение.
    2. Написать декоратор из п.1, но с параметром – путь к логам.
    3. Применить написанный логгер к приложению из любого предыдущего д/з.

---
+++ Advanced lesson 5 decorators/main.py
@@ -0,0 +1,28 @@
+import datetime
+import os
+import requests
+
+LOG_FILE = os.path.join(os.getcwd(), 'log')
+
+def log_decor_param(log_path):
+    def log_decor(old_function):
+        def new_function(*args, **kwargs):
+            exec_time = datetime.datetime.now()
+            result = old_function(*args, **kwargs)
+            with open(log_path, 'a', encoding='UTF-8') as f:
+                if os.stat(log_path).st_size == 0:
+                    f.writelines('exec_date,function_name,args,kwargs,function_result\n')
+                f.writelines(f'{exec_time},{old_function.__name__},{args},{kwargs},{result}\n')
+            return result
+        return new_function
+    return log_decor
+
+@log_decor_param(LOG_FILE+'1')
+def get_smartest_hero(*hero_list):
+    hero_dict_list = []
+    for hero in hero_list:
+        r = requests.get(f'https://superheroapi.com/api/2619421814940190/search/{hero}').json()
+        hero_dict_list.append({'name': r['results'][0]['name'], 'intelligence': int(r['results'][0]['powerstats']['intelligence'])})
+    return f'Самый умный герой - {max(hero_dict_list, key=lambda k: k["intelligence"])["name"]}'
+
+print(get_smartest_hero('hulk', 'captain america', 'thanos'))
